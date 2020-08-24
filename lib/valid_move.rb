d70a96419e34964523767ce65b1c614010a880a6
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 06:02:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between(1,8) && position_taken?(board, index) == true
+  if index >= 0 && index <= 8 && position_taken?(board, index) == true
     true
   else
     false
